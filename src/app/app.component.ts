e819ea772067f31a1c7558b1698d37710aa6c9c7
Javijefe <javgugar@alu.edu.gva.es>
2025-02-15 19:55:56 +0100

    acabado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,  FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Calculadora';
+  title = 'Mini Calculadora';
+  numero1:number=0;
+  numero2:number=0;
+  resultado:number=0;
+
+  sumar():void{
+
+    this.resultado=this.numero1+this.numero2;
+
+  }
+
+  restar():void{
+    this.resultado=this.numero1-this.numero2;
+
+  }
+  
 }
